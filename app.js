a6c41e6474d1fb70a81391c8b2cc30559710d7d0
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-05-26 23:43:56 +0500

    js Assignment commmit1

---
+++ app.js
@@ -0,0 +1,53 @@
+//***************************** Chapter : 26-30 ************************** */
+
+
+//***************************** Task : 01 ************************** */
+
+// var userInput = prompt("Enter positive integer");
+// var roundValue = Math.round(userInput)
+// var floorValue = Math.floor(userInput)
+// var ceilValue = Math.ceil(userInput)
+
+// document.write("number : " + userInput +"<br>"+
+//                 "round off value : " + roundValue +"<br>"+
+//                 "floor value : " + floorValue +"<br>"+
+//                 "ceil value : " + ceilValue)
+
+
+//***************************** Task : 02 ************************** */
+
+// var userInput = prompt("Enter negative floating point number");
+// var roundValue = Math.round(userInput)
+// var floorValue = Math.floor(userInput)
+// var ceilValue = Math.ceil(userInput)
+
+// document.write("number : " + userInput +"<br>"+
+//                 "round off value : " + roundValue +"<br>"+
+//                 "floor value : " + floorValue +"<br>"+
+//                 "ceil value : " + ceilValue)
+
+
+
+//***************************** Task : 04 ************************** */
+
+// var randomNum = Math.random()*6;
+// var roundNum = Math.round(randomNum);
+// document.write("random dice value : " + roundNum)
+            
+
+//***************************** Task : 06 ************************** */
+
+// var randomNum = Math.random()*100;
+// var roundNum = Math.round(randomNum);
+// document.write("random number between 1 and 100 : " + roundNum)
+   
+
+//***************************** Task : 08 ************************** */
+
+// var secretNum = 5;
+// var userInput = +prompt("Enter a number");
+// if(userInput === secretNum){
+//     alert("Congratulation! your number is matched")
+// }else{
+//     alert("Try again!")
+// }
